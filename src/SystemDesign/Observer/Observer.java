bd18af3354731e26609f6c4872b2ff696fe85404
ujjwal2198 <43241568+ujjwal2198@users.noreply.github.com>
2024-04-06 23:54:26 +0530

    Update Observer.java

--- src/SystemDesign/Observer/Observer.java
+++ src/SystemDesign/Observer/Observer.java
@@ -1,1 +1,8 @@
-
+package SystemDesign.ObserverPattern;
+  
+public interface Obserable{
+  public abstract void addObserver(Observer observer);
+  public abstract void removeObserver(Observer observer);
+  public abstract void notifyObservers();
+  public abstract void updateNotifyMe(int quantity);
+}
