c88badb361fd0d0b6c897c24f90c87ac46d591dd
ujjwal2198 <43241568+ujjwal2198@users.noreply.github.com>
2024-04-06 19:03:58 +0530

    Update FourWheeler.java

--- src/SystemDesign/StrategyPattern/FourWheeler.java
+++ src/SystemDesign/StrategyPattern/FourWheeler.java
@@ -1,7 +1,7 @@
 package SystemDesign.StrategyPattern;
 
 public class FourWheeler implements WheelerStrategy {
-  public void applyAccerate(){
+  public void applyAccelerate(){
     System.out.println("car Accerate is ON");
   }
 }
